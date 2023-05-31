3064a28728bed0c8024080f0d389859292388c7b
dendyindriyo <dendyindriyo@gmail.com>
2023-05-31 18:45:44 +0700

    Adding TSuite_01, TSuite_02

---
+++ Test Suites/TSuite_01_Positive.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSuite_01_Positive</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>75f42f4a-1eb1-40ba-8fc4-18ca8811e586</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/TSuite_02_Negative.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSuite_02_Negative</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>310985d1-1cb6-4408-8b02-8b35ea1d1658</testSuiteGuid>
+</TestSuiteEntity>
